65978138e683aeb863eea43213f92c8e128fad93
Lerogero <Lerogero@gmail.com>
2015-05-15 02:57:47 +0100

    Finished Game

--- myJavaScriptFile.js
+++ myJavaScriptFile.js
@@ -1,21 +1,15 @@
 // myJavaScriptFile.js
 
-// Set the initialise function to be called
-// when the page has loaded.
-
 window.onload = init;
 
-// set the size of our canvas / view onto the scene.
 var WIDTH = window.innerWidth;
 var HEIGHT =  window.innerHeight;
 
-// set camera properties / attributes.
 var VIEW_ANGLE = 45;
 var ASPECT_RATIO = WIDTH / HEIGHT;
 var NEAR_CLIPPING_PLANE = 0.1;
 var FAR_CLIPPING_PLANE = 10000;
 
-// Declare the variables we will need for the three.js
 var renderer;
 var scene;
 var camera;
@@ -26,9 +20,7 @@ var keyboard = new THREEx .KeyboardState();
 
 var keyboard;
 
-var raycaster;
-var raycasterFront;
-var raycasterBack;
+var raycaster, raycasterFront, raycasterBack;
 
 var objects = [];
 
@@ -37,22 +29,14 @@ var yAcceleration = 2.0;
 
 var myColladaLoader;
 var terrain;
-var arrow;
-var arrowTwo;
-var arrowThree;
-var arrowFour;
-var arrowFive;
-var arrowSix;
+var arrow, arrowTwo, arrowThree, arrowFour, arrowFive, arrowSix;
 
 var skyBoxMesh;
 var texture_placeholder;
 
-
-
 function init() {
 
   renderer = new THREE.WebGLRenderer();
-
 
   var docElement = document.getElementById('myDivContainer');
   docElement.appendChild(renderer.domElement);
@@ -60,7 +44,7 @@ function init() {
   renderer.setClearColor("rgb(135,206,250)");
 
   renderer.setSize( window.innerWidth, window.innerHeight );
-  // Set the rednerer size for window rescale.
+
   window.addEventListener( 'resize', onWindowResize, false );
   function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -68,7 +52,6 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
   }
 
-  // Stats of performance of game
   stats = new Stats();
   stats.domElement.style.position = 'absolute';
   stats.domElement.style.top = '0px';
@@ -91,10 +74,8 @@ function init() {
   render();
 }
 
-
 function initScene() {
 
-  // Skybox
   texture_placeholder = document.createElement( 'canvas' );
   texture_placeholder.width = 128;
   texture_placeholder.height = 128;
@@ -104,19 +85,14 @@ function initScene() {
   context.fillRect( 0 , 0, texture_placeholder.width, texture_placeholder.height );
 
   var materials = [
-    loadTexture( ' textures/cube/skybox/Left.png' ),
-    loadTexture( ' textures/cube/skybox/Right.png' ),
-    loadTexture( ' textures/cube/skybox/Up.png' ),
-    loadTexture( ' textures/cube/skybox/Down.png' ),
-    loadTexture( ' textures/cube/skybox/Back.png' ),
-    loadTexture( ' textures/cube/skybox/Front.png' )
+    loadTexture( ' textures/cube/skybox/Left.png' ), loadTexture( ' textures/cube/skybox/Right.png' ), loadTexture( ' textures/cube/skybox/Up.png' ),
+    loadTexture( ' textures/cube/skybox/Down.png' ), loadTexture( ' textures/cube/skybox/Back.png' ), loadTexture( ' textures/cube/skybox/Front.png' )
   ];
 
 skyBoxMesh = new THREE.Mesh( new THREE.BoxGeometry( 10000, 10000, 10000, 7, 7, 7), new THREE.MeshFaceMaterial( materials ));
 skyBoxMesh.scale.x = - 1;
 scene.add(skyBoxMesh);
 
-  // Add light
   var light = new THREE.DirectionalLight( 0xffffff, 1.5 ,1.5 );
   light.position.set( 1, 1, 1 );
   scene.add( light );
@@ -128,19 +104,15 @@ scene.add(skyBoxMesh);
   myColladaLoader = new THREE.ColladaLoader();
   myColladaLoader.options.convertUpAxis = true;
 
-  // Import arrow model
 	myColladaLoader.load( 'arrow.DAE', function ( collada ) {
-			// Here we store the dae in a global variable.
+
 			arrow = collada.scene;
 
-			// Position your model in the scene (world space).objects
       arrow.position.set(170,-4.5,225);
       arrow.rotation.y = 90 * Math.PI / 180;
-			// Scale your model to the correct size.
+
       arrow.scale.x = arrow.scale.y = arrow.scale.z = 0.3;
       arrow.updateMatrix();
-
-			// Add the model to the scene.
 
       arrow.name = "arrow";
 			scene.add(arrow);
@@ -148,19 +120,15 @@ scene.add(skyBoxMesh);
 
 		} );
 
-    // Import arrow model
     myColladaLoader.load( 'arrow.DAE', function ( collada ) {
-        // Here we store the dae in a global variable.
+
         arrowTwo = collada.scene;
 
-        // Position your model in the scene (world space).objects
         arrowTwo.position.set(0,-4.5,225);
         arrowTwo.rotation.y = 0 * Math.PI / 180;
-        // Scale your model to the correct size.
+
         arrowTwo.scale.x = arrowTwo.scale.y = arrowTwo.scale.z = 0.3;
         arrowTwo.updateMatrix();
-
-        // Add the model to the scene.
 
         arrowTwo.name = "arrowTwo";
         scene.add(arrowTwo);
@@ -169,17 +137,14 @@ scene.add(skyBoxMesh);
       } );
 
       myColladaLoader.load( 'arrow.DAE', function ( collada ) {
-          // Here we store the dae in a global variable.
+
           arrowThree = collada.scene;
 
-          // Position your model in the scene (world space).objects
           arrowThree.position.set(0,-4.5,40);
           arrowThree.rotation.y = 270 * Math.PI / 180;
-          // Scale your model to the correct size.
+
           arrowThree.scale.x = arrowThree.scale.y = arrowThree.scale.z = 0.3;
           arrowThree.updateMatrix();
-
-          // Add the model to the scene.
 
           arrowThree.name = "arrowThree";
           scene.add(arrowThree);
@@ -188,17 +153,14 @@ scene.add(skyBoxMesh);
         } );
 
         myColladaLoader.load( 'arrow.DAE', function ( collada ) {
-            // Here we store the dae in a global variable.
+
             arrowFour = collada.scene;
 
-            // Position your model in the scene (world space).objects
             arrowFour.position.set(120,-4.5,40);
             arrowFour.rotation.y = 270 * Math.PI / 180;
-            // Scale your model to the correct size.
+
             arrowFour.scale.x = arrowFour.scale.y = arrowFour.scale.z = 0.3;
             arrowFour.updateMatrix();
-
-            // Add the model to the scene.
 
             arrowFour.name = "arrowFour";
             scene.add(arrowFour);
@@ -207,17 +169,14 @@ scene.add(skyBoxMesh);
           } );
 
           myColladaLoader.load( 'arrow.DAE', function ( collada ) {
-              // Here we store the dae in a global variable.
+
               arrowFive = collada.scene;
 
-              // Position your model in the scene (world space).objects
               arrowFive.position.set(260,-4.5,40);
               arrowFive.rotation.y = 0 * Math.PI / 180;
-              // Scale your model to the correct size.
+
               arrowFive.scale.x = arrowFive.scale.y = arrowFive.scale.z = 0.3;
               arrowFive.updateMatrix();
-
-              // Add the model to the scene.
 
               arrowFive.name = "arrowFive";
               scene.add(arrowFive);
@@ -226,17 +185,14 @@ scene.add(skyBoxMesh);
             } );
 
             myColladaLoader.load( 'arrow.DAE', function ( collada ) {
-                // Here we store the dae in a global variable.
+
                 arrowSix = collada.scene;
 
-                // Position your model in the scene (world space).objects
                 arrowSix.position.set(260,-4.5,-60);
                 arrowSix.rotation.y = 90 * Math.PI / 180;
-                // Scale your model to the correct size.
+
                 arrowSix.scale.x = arrowSix.scale.y = arrowSix.scale.z = 0.3;
                 arrowSix.updateMatrix();
-
-                // Add the model to the scene.
 
                 arrowSix.name = "arrowSix";
                 scene.add(arrowSix);
@@ -244,21 +200,19 @@ scene.add(skyBoxMesh);
 
               } );
 
-    // Import terrain model
+
     myColladaLoader.load( 'room.DAE', function ( collada ) {
-        // Here we store the dae in a global variable.
+
         terrain = collada.scene;
 
-        // Position your model in the scene (world space).
         terrain.position.x = 0;
         terrain.position.y = -5;
         terrain.position.z = 0;
 
-        // Scale your model to the correct size.
         terrain.scale.x = terrain.scale.y = terrain.scale.z = 0.5;
         terrain.updateMatrix();
 
-        // Add the model to the scene.
+
         terrain.name = "terrain";
         scene.add(terrain);
         objects.push(terrain);
@@ -273,24 +227,21 @@ function render() {
 
   var tmpY = camera.position.y;
 
-  // Check camera rotation
   var matrix = new THREE.Matrix4();
   matrix.extractRotation( camera.matrix );
-  var direction = new THREE.Vector3( 0,0,-1);
-  direction = direction.applyMatrix4(matrix);
+  var trajectory = new THREE.Vector3( 0,0,-1);
+  trajectory = trajectory.applyMatrix4(matrix);
 
-  var directionB = new THREE.Vector3(0,0,-1).negate();
-  directionB = directionB.applyMatrix4(matrix);
+  var trajectoryBack = new THREE.Vector3(0,0,-1).negate();
+  trajectoryBack = trajectoryBack.applyMatrix4(matrix);
 
-  // Raycaster in direction of front and back of camera
-  raycasterFrontC = new THREE.Raycaster( camera.position,direction,0,2);
+  raycasterFrontC = new THREE.Raycaster( camera.position,trajectory,0,2);
   var intersectionsFront = raycasterFrontC.intersectObjects( objects, true);
-  raycasterBackC = new THREE.Raycaster( camera.position, directionB,0,2);
+  raycasterBackC = new THREE.Raycaster( camera.position, trajectoryBack,0,2);
   var intersectionsBack = raycasterBackC.intersectObjects( objects, true);
 
   camera.position.y = tmpY;
 
-  // Raycaster gravity down
   raycaster.ray.origin.copy( camera.position );
   raycaster.ray.y -= 1;
   var intersections = raycaster.intersectObjects( objects, true );
@@ -299,7 +250,6 @@ function render() {
 
   camera.position.y = camera.position.y = yVelocity;
 
-  //Debugger
   document.getElementById("debugInfo").innerHTML =  "Gravity = " + intersections.length +
     "<br>" +"Front Collision = " + intersectionsFront.length +
     "<br>" + "Back Collision = " + intersectionsBack.length;
@@ -320,7 +270,6 @@ function render() {
       }
     }
 
-    // Movement controls
     if( keyboard.pressed("W")) {
       if(intersectionsFront.length > 0 ) {
         moveDistance = 0;
@@ -350,7 +299,6 @@ function render() {
 
 }
 
-// Load the skybox textures
 function loadTexture( path ) {
   var texture = new THREE.Texture( texture_placeholder );
   var material = new THREE.MeshBasicMaterial( {map: texture, overdraw: 0.5 });
@@ -366,3 +314,7 @@ function loadTexture( path ) {
 
   return material;
 }
+
+//-------------------------------------------------
+//                          EOF
+//-------------------------------------------------
